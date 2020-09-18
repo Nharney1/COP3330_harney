2017bafaefcd105f9b56ae8a787e20f5216e1414
Noah <nharney1@knights.ucf.edu>
2020-09-18 14:05:51 -0400

    First Commit for p2

---
+++ harney_p2/src/App.java
@@ -0,0 +1,105 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args){
+        ArrayList<BodyMassIndex> bmiData = new ArrayList<BodyMassIndex> ();
+
+        while (moreInput()){
+            double height = getUserHeight();
+            double weight = getUserWeight();
+
+            BodyMassIndex bmi = new BodyMassIndex(height, weight);
+            bmiData.add(bmi);
+
+            displayBmiInfo(bmi);
+        }
+
+        displayBmiStatistics(bmiData);
+    }
+
+    private static boolean moreInput(){
+        String userInput = getUserInput();
+
+        return userInput.equalsIgnoreCase("y");
+    }
+
+    private static String getUserInput(){
+        System.out.println("Would you like to enter more data? Y or N: ");
+        Scanner input = new Scanner(System.in);
+        String userAnswer = input.next();
+        return userAnswer;
+
+    }
+
+    private static double getUserHeight(){
+        System.out.println("Please enter the height of the person in inches: ");
+        Scanner input = new Scanner(System.in);
+        double userHeightInput= input.nextDouble();
+        input.nextLine();
+
+
+        double verifiedHeight = verifyHeightInput(userHeightInput);
+        return verifiedHeight;
+    }
+
+    private static double verifyHeightInput(double userHeightInput){
+        if(userHeightInput >= 0){
+            return userHeightInput;
+        }
+        else{
+            System.out.println("Pleas enter a positive value for the height \n");
+            double newHeightInput = getUserHeight();
+            return newHeightInput;
+        }
+
+    }
+
+    private static double getUserWeight() {
+        System.out.println("Please enter the weight of the person in pounds: ");
+        Scanner input = new Scanner(System.in);
+        double userWeightInput = input.nextDouble();
+        input.nextLine();
+
+        double verifiedWeight = verifyWeightInput(userWeightInput);
+        return verifiedWeight;
+    }
+
+    private static double verifyWeightInput(double userWeightInput){
+        if(userWeightInput >= 0){
+            return userWeightInput;
+        }
+        else{
+            System.out.println("Pleas enter a positive value for the height \n ");
+            double newWeightInput = getUserWeight();
+            return newWeightInput;
+
+        }
+    }
+
+    public static void displayBmiInfo(BodyMassIndex bmi){
+
+        System.out.println("This person has a BMI of: " + bmi.getBmi() + " \n");
+        System.out.println("This person would be classified as: " + bmi.getCategory() + "\n");
+
+    }
+
+    public static void displayBmiStatistics(ArrayList<BodyMassIndex> bmiData){
+        double bmiTotal =0;
+        int counter=0;
+        for(BodyMassIndex eachBmiElement: bmiData){
+            bmiTotal += eachBmiElement.bmi;
+            counter++;
+        }
+
+        if (counter != 0){
+            System.out.println("The average BMI for the inputted data is: " + bmiTotal/counter);
+        }
+        else {
+            System.out.println("No BMI data was entered. The average BMI cannot be computed.");
+        }
+    }
+}
+
+
---
+++ harney_p2/src/BodyMassIndex.java
@@ -0,0 +1,41 @@
+public class BodyMassIndex {
+
+    public double bmi;
+    private String bmiCategory;
+
+    public BodyMassIndex(double heightInput, double weightInput) {
+        double height = heightInput;
+        double weight = weightInput;
+
+        bmi = bmiCalculator(height, weight);
+
+        bmiCategory = category(bmi);
+
+    }
+
+    public double bmiCalculator(double height, double weight) {
+        double bmiCalculation = 703 * weight / (height * height);
+        return bmiCalculation;
+    }
+
+    public String category(double inputBmi) {
+        if (bmi >= 30) {
+            return "Obesity";
+        } else if (bmi >= 25) {
+            return "Overweight";
+        } else if (bmi > 18.5) {
+            return "Normal Weight";
+        } else
+            return "Underweight";
+
+    }
+
+    public double getBmi (){
+        return bmi;
+    }
+
+    public String getCategory(){
+        return bmiCategory;
+    }
+
+}
